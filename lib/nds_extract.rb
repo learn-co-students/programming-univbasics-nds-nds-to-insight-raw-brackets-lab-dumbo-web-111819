85e23edc811f79b2d2d4d8b288f31653781c9c05
Ryan Troy Lattibeaudiere <r_latti@hotmail.com>
2019-11-06 21:52:33 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # pp nds
+   pp nds
   # movies = nds[0][:worldwide_gross]
   counter = 0 
   total = {}
