0486300e9802255873b1f7c7d46bf2866be733fd
Yu Hao Ye <rxye1991@gmail.com>
2019-11-16 22:36:47 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,5 +18,96 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+director_names = nds[1][:name]
+result[director_names] = 0
+i = 0
+director_names = nds[0][:name]
+result[director_names] = 0
+while i < 6 do 
+    result[director_names] = result[director_names] + nds[0][:movies][i][:worldwide_gross]
+    i += 1 
 end
+result 
+director_names = nds[1][:name]
+result[director_names] = 0
+
+i = 0 
+while i < 6 do 
+    result[director_names] = result[director_names] + nds[1][:movies][i][:worldwide_gross]
+    i += 1 
+end
+  result 
+  
+  
+  director_names = nds[2][:name]
+result[director_names] = 0
+
+i = 0 
+while i < 7 do 
+    result[director_names] = result[director_names] + nds[2][:movies][i][:worldwide_gross]
+    i += 1 
+end
+  result 
+  
+director_names = nds[3][:name]
+result[director_names] = 0
+
+i = 0 
+while i < 5 do 
+    result[director_names] = result[director_names] + nds[3][:movies][i][:worldwide_gross]
+    i += 1 
+end
+  result 
+  
+director_names = nds[4][:name]
+result[director_names] = 0
+
+i = 0 
+while i < 6 do 
+    result[director_names] = result[director_names] + nds[4][:movies][i][:worldwide_gross]
+    i += 1 
+end
+  result 
+  
+director_names = nds[5][:name]
+result[director_names] = 0
+
+i = 0 
+while i < 6 do 
+    result[director_names] = result[director_names] + nds[5][:movies][i][:worldwide_gross]
+    i += 1 
+end
+  result 
+
+director_names = nds[6][:name]
+result[director_names] = 0
+
+i = 0 
+while i < 6 do 
+    result[director_names] = result[director_names] + nds[6][:movies][i][:worldwide_gross]
+    i += 1 
+end
+  result 
+
+director_names = nds[7][:name]
+result[director_names] = 0
+
+i = 0 
+while i < 6 do 
+    result[director_names] = result[director_names] + nds[7][:movies][i][:worldwide_gross]
+    i += 1 
+end
+  result 
+  
+director_names = nds[8][:name]
+result[director_names] = 0
+
+i = 0 
+while i < 6 do 
+    result[director_names] = result[director_names] + nds[8][:movies][i][:worldwide_gross]
+    i += 1 
+end
+  result 
+  
+  
+end 
